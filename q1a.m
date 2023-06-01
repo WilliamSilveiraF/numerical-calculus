620c8a859f2e0973fa4c193674787a36ef2b143a
William Silveira <williamsilveiraf@hotmail.com>
2023-06-01 15:07:23 -0300

    chore: plot responses

--- q1a.m
+++ q1a.m
@@ -1,3 +1,6 @@
+% Q1 - A
+fprintf('Q1 - A \n');
+
 xi = pi/4; %Ponto médio do intervalo
 x = [0:xi:pi/2];
 erro_max = 0.01; % precisão
@@ -5,11 +8,20 @@ erro_max = 0.01; % precisão
 [y, coef] = x_sen_x(x, xi, erro_max); % aproxime por Série de Taylor, expandida a partir do ponto médio do intervalo, xi=pi/4. 
 y_exato = x .* sin(x);
 
+q1_a_erro_local = abs(y - y_exato)
 [erro_max_real, pos_erroMax] = max(abs(y - y_exato))
 
 % Imprima os coeficientes da expansão
-fprintf('Os coeficientes da expansão são:\n');
+fprintf('Os coeficientes são:\n');
 disp(coef);
 
+
+figure;
 plot(x, y, "- r", x, y_exato, "- b", xi, 0, "* k");
+title('Q1A -  - Aproximação x Exato');
+grid on;
+
+figure;
+plot(x, q1_a_erro_local, 'k-');
+title('Q1A - Erro Local');
 grid on;
--- q1b.m
+++ q1b.m
@@ -1,21 +1,25 @@
+
 % Q1 - B
-fprintf('Q1 - B \n');
+fprintf('\n\nQ1 - B \n');
 
 #Aproxime a função de f(x) com erro menor que 1e-2
 #f(x) = x * sin(x)
 f = @(x) x.*sin(x);
 a = 0;
 b = pi/2;
-n = 3
-h = (b - a)/n;
+grau_n = 3
+h = (b - a)/grau_n;
 x = [a : h : b];
-c = coeficientes_greg_nwtn(n, a, b, f);
+c = coeficientes_greg_nwtn(grau_n, a, b, f);
 
 x_plot = [a : h/10 : b];
-y_plot = func_pol_gn(n, c, x, x_plot);
+y_plot = func_pol_gn(grau_n, c, x, x_plot);
 y_exato = f(x_plot);
 erro_local = abs(y_plot - y_exato);
 [erro_max, pos_erro] = max(abs(y_plot - y_exato))
+
+fprintf('Os coeficientes são: \n');
+disp(c);
 
 figure;
 plot(x_plot, y_plot, 'b-', x_plot, y_exato, 'r-');
@@ -28,3 +32,8 @@ figure;
 plot(x_plot, erro_local, 'k-');
 title('Q1B - Erro Local');
 grid on;
+
+% Q2 - B
+fprintf('\n\nQ1 - C \n');
+fprintf('A melhor aproximação foi por Série de Taylor que obteve o menor erro máximo.\n');
+fprintf('A aproximação mais eficiente foi por interpolação de Gregory-Newton que obteve o menor grau n.\n');
